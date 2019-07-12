8aecd8b4bff6e9b2358f5c747927551f5c2d0fee
davidliu <david-khala@hotmail.com>
2019-07-12 13:57:51 +0800

    add ssh.sh: RSAKeyGen

---
+++ ssh.sh
@@ -0,0 +1,30 @@
+genRSA() {
+    local keySize
+    local email
+    local rsaKeyPrv
+    read -p "enter email (default:david-khala@hotmail.com):" email
+    read -p "enter keySize (default:4096) :" keySize
+    keySize=${keySize:-4096}
+    email=${email:-"david-khala@hotmail.com"}
+    ssh-keygen -t rsa -b $keySize -C $email
+    eval "$(ssh-agent -s)"
+
+
+
+    if [[ -f ~/.ssh/config ]];then
+        local content="
+        Host *
+            AddKeysToAgent yes
+            UseKeychain yes
+            IdentityFile ~/.ssh/id_rsa
+        "
+        echo ${content} >> ~/.ssh/config
+    fi
+    read -p "Enter key file path again ($HOME/.ssh/id_rsa):" rsaKeyPrv
+    rsaKeyPrv=${rsaKeyPrv:-"$HOME/.ssh/id_rsa"}
+    ssh-add -K $rsaKeyPrv
+}
+copyRSAPub() {
+    pbcopy < ~/.ssh/id_rsa.pub
+}
+$1
