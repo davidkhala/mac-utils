ee7abcb109a51924ddaba97ca7cfdf6715316915
davidliu <david-khala@hotmail.com>
2020-07-13 17:07:43 +0800

    [DEBUG] travis

--- brew.sh
+++ brew.sh
@@ -1,10 +1,11 @@
 set -e
 install() {
-    if ! brew config >/dev/null; then
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
+	if ! brew config >/dev/null; then
+		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	else
+		echo "brew exists already"
+	fi
 }
 if [[ $(uname) == "Darwin" ]]; then
-    $1
+	$1
 fi
-
--- ssh.sh
+++ ssh.sh
@@ -1,33 +1,31 @@
 set -e
 genRSA() {
-    local keySize
-    local email
-    local rsaKeyPrv
-    read -p "enter email (default:david-khala@hotmail.com):" email
-    read -p "enter keySize (default:4096) :" keySize
-    keySize=${keySize:-4096}
-    email=${email:-"david-khala@hotmail.com"}
-    ssh-keygen -t rsa -b $keySize -C $email
-    eval "$(ssh-agent -s)"
+	local keySize
+	local email
+	local rsaKeyPrv
+	read -p "enter email (default:david-khala@hotmail.com):" email
+	read -p "enter keySize (default:4096) :" keySize
+	keySize=${keySize:-4096}
+	email=${email:-"david-khala@hotmail.com"}
+	ssh-keygen -t rsa -b $keySize -C $email
+	eval "$(ssh-agent -s)"
 
-
-
-    if [[ -f ~/.ssh/config ]];then
-        local content="
+	if [[ -f ~/.ssh/config ]]; then
+		local content="
         Host *
             AddKeysToAgent yes
             UseKeychain yes
             IdentityFile ~/.ssh/id_rsa
         "
-        echo ${content} >> ~/.ssh/config
-    fi
-    read -p "Enter key file path again ($HOME/.ssh/id_rsa):" rsaKeyPrv
-    rsaKeyPrv=${rsaKeyPrv:-"$HOME/.ssh/id_rsa"}
-    ssh-add -K $rsaKeyPrv
+		echo ${content} >>~/.ssh/config
+	fi
+	read -p "Enter key file path again ($HOME/.ssh/id_rsa):" rsaKeyPrv
+	rsaKeyPrv=${rsaKeyPrv:-"$HOME/.ssh/id_rsa"}
+	ssh-add -K $rsaKeyPrv
 }
 copyRSAPub() {
-    pbcopy < ~/.ssh/id_rsa.pub
+	pbcopy <~/.ssh/id_rsa.pub
 }
 if [[ $(uname) == "Darwin" ]]; then
-    $1
+	$1
 fi
