aa1d4b76b7971f4be096792600e3f131033c9a58
davidliu <david-khala@hotmail.com>
2020-07-13 16:55:04 +0800

    add badge align standard

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mac-utils
 MacOS developer utils
+[![Build Status](https://travis-ci.com/davidkhala/mac-utils.svg?branch=master)](https://travis-ci.com/davidkhala/mac-utils)
--- brew.sh
+++ brew.sh
@@ -1,3 +1,4 @@
+set -e
 install() {
     if ! brew config >/dev/null; then
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
