7bc482e24cf608902b01275c36e3f6bee22fa24f
David Liu <david.yx.liu@oracle.com>
2021-08-12 16:53:22 +0800

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,9 @@
+install(){
+  brew cask install docker
+  open -a Docker
+}
+installCompose() {
+      echo "Docker Desktop for Mac includes Compose along with other Docker apps, so Mac users do not need to install Compose separately."
+			echo "More details: https://docs.docker.com/docker-for-mac/install/"
+			exit 0
+}
