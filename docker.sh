28c802369afade3de82270d2839eb2412e6d057d
David Liu <david.yx.liu@oracle.com>
2021-08-12 17:18:05 +0800

    touch docker and mysql

--- docker.sh
+++ docker.sh
@@ -1,10 +1,10 @@
-install(){
+install() {
   brew cask install docker
   open -a Docker
 }
 installCompose() {
-      echo "Docker Desktop for Mac includes Compose along with other Docker apps, so Mac users do not need to install Compose separately."
-			echo "More details: https://docs.docker.com/docker-for-mac/install/"
-			exit 0
+  echo "Docker Desktop for Mac includes Compose along with other Docker apps, so Mac users do not need to install Compose separately."
+  echo "More details: https://docs.docker.com/docker-for-mac/install/"
+  exit 0
 }
 $@
