51e5ba2c8280df1c4fcce968cb1926418c738beb
David Liu <david.yx.liu@oracle.com>
2021-08-12 17:26:30 +0800

    add installer.yml
    Signed-off-by: David Liu <david.yx.liu@oracle.com>

--- docker.sh
+++ docker.sh
@@ -1,10 +1,8 @@
 install() {
-  brew cask install docker
+  brew install --cask docker
   open -a Docker
 }
 installCompose() {
-  echo "Docker Desktop for Mac includes Compose along with other Docker apps, so Mac users do not need to install Compose separately."
-  echo "More details: https://docs.docker.com/docker-for-mac/install/"
-  exit 0
+  brew install docker-compose
 }
 $@
